0b84a3165f296cf40a83771a2d2943ed5456e827
Sherlock.Yu <767954505@qq.com>
2015-02-04 21:54:24 -0800

    Swift_Project
    3 Projects,2 By Myself

---
+++ AncientWordsGuessingGame/ChooseMenuViewController.swift
@@ -0,0 +1,35 @@
+//
+//  ChooseMenuViewController.swift
+//  AncientWordsGuessingGame
+//
+//  Created by Sherlock.Yu on 15-2-4.
+//  Copyright (c) 2015年 Sherlock.Yu. All rights reserved.
+//
+
+import UIKit
+
+class ChooseMenuViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
